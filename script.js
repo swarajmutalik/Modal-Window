049c080036ddd3f81b897490337c3b6633d54483
swarajmutalik <swarajmutalik@gmail.com>
2025-07-08 11:54:26 +0530

    Esc Key functionality added

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const openModal = function () {
-  console.log("Button CLicked");
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
@@ -22,3 +21,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
